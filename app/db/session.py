2463f2568e695cfdf74c50499c64399404636a44
Bernarr-77 <Bernarrnarciso@outlook.com>
2026-03-20 15:26:54 -0300

    Conexão com o postgresSQl pronta

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
+class Base(DeclarativeBase):
+    pass
+
 engine = create_engine(os.getenv("DATABASE_URL"))
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
